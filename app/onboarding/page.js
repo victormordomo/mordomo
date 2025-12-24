995626d1db0b39c7974ee291969749a8332c084e
victormordomo <alphareviveman@gmail.com>
2025-12-24 10:36:26 -0300

    Update page.js

--- app/onboarding/page.js
+++ app/onboarding/page.js
@@ -1,8 +1,8 @@
 'use client'
 
 import { useState } from 'react'
-import { supabase } from '../lib/supabase'
 import { useRouter } from 'next/navigation'
+import { supabase } from '../lib/supabase'
 
 export default function Home() {
   const [email, setEmail] = useState('')
