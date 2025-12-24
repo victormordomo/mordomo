f0e20f3ac0d995f26a6b8ee0be4fd6354a9d7f29
victormordomo <alphareviveman@gmail.com>
2025-12-24 09:03:53 -0300

    Create page.js

---
+++ app/page.js
@@ -0,0 +1,10 @@
+'use client'
+
+export default function Home() {
+  return (
+    <div style={{ padding: 40 }}>
+      <h1>Mordomo</h1>
+      <p>Seu sistema de mordomia cristã</p>
+    </div>
+  )
+}
