ce6cdfcc3a4ce7d6b26ad183b0cbecc11b1ab8f9
victormordomo <alphareviveman@gmail.com>
2025-12-24 09:49:44 -0300

    Update page.js

--- app/page.js
+++ app/page.js
@@ -5,6 +5,7 @@ export default function Home() {
     <div style={{ padding: 40 }}>
       <h1>Mordomo</h1>
       <p>Seu sistema de mordomia cristã</p>
+      <p>Login em breve...</p>
     </div>
   )
 }
