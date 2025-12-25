81df5619f94c17a38e7f3c55889e6b3721486de4
victormordomo <alphareviveman@gmail.com>
2025-12-25 14:13:29 -0300

    Create page.js

---
+++ app/transacoes/page.js
@@ -0,0 +1,10 @@
+'use client'
+
+export default function Transacoes() {
+  return (
+    <div style={{ padding: 40 }}>
+      <h1>Transações</h1>
+      <p>Aqui você vai adicionar suas entradas e saídas.</p>
+    </div>
+  )
+}
